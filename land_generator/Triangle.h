dfe3feeadae2e37f0216d0f706c7edc402631e99
danilfilippov93 <danilfilippov93@gmail.com>
2013-04-12 10:18:21 +0400

    Paint triangle [WIP]

---
+++ land_generator/Triangle.h
@@ -0,0 +1,14 @@
+#ifndef Triangle_H
+#define Triangle_H
+
+class Triangle {
+    public:
+    Triangle();
+    void Draw();
+    void Init();
+    
+    unsigned int vertexArrayID;
+    unsigned int vertexBuffer;
+};
+
+#endif
